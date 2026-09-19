6ab1f74d97bcbb5573773fb3f9296e3a6fc33cec
Ricardo Reyes <ricardo.reyes@utexas.edu>
2026-09-18 21:55:42 -0500

    "set up the basic timer"

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 let http = require('http');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World!');
 }).listen(8080);
+
