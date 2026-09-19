c5ff97cf6ba0fd4f5c933de6e61cfcc66c9664c4
the-colonel-sanders <155794210+the-colonel-sanders@users.noreply.github.com>
2026-09-19 00:45:52 -0500

    Vite setup and file reorganization
    Vite established in project directory, with almost all files intact. Major reorganization, with a new web folder to encompass all of our website work, as well as index.html lying outside of public or src. (Please keep this in mind next time you are referencing file paths.) However, css, timer functionality, and website still all work fine, just use npm run dev in project folder.\web to run the website.

---
+++ web/src/App.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+export default function App() {
+    const [timeLeft, setTimeLeft] = React.useState(1500);
+    const [isRunning, setIsRunning] = React.useState(false);
+
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    const formattedSeconds = String(seconds).padStart(2, "0");
+
+
+    React.useEffect(() => {
+        console.log("Timer effect started");
+
+        if (isRunning) {
+            const interval = setInterval(() => {
+                setTimeLeft(timeLeft => timeLeft - 1);
+            }, 1000)
+
+            return () => {
+                clearInterval(interval);
+            };
+        }
+
+
+    }, [isRunning]);
+
+    return (
+        <div>
+            <h1 className="heading">UT Native Pomodoro</h1>
+            <p className="timer">{minutes}:{formattedSeconds}</p>
+            <div className="subtractButton">
+                <button className="minus" onClick={() => setIsRunning(true)}>
+                    subtract 1
+                </button>
+                <button className="stop" onClick={() => setIsRunning(false)}>
+                    stop
+                </button>
+            </div>
+
+        </div>
+
+    );
+
+
+}
+
+const root = createRoot(
+    document.getElementById("root")
+);
+
+root.render(<App />);
